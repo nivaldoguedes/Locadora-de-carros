a28621d85790add1501c197e8944a8c98fbf5343
Ruslan <ruslan.andruscha.dev@gmail.com>
2025-11-09 14:01:18 -0300

    Criado view menu com MenuController e pequenas alterações em login

---
+++ src/controller/MenuController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button botaoMenuSair;
+
+    @FXML
+    private Button btnMenuCadastrarCliente;
+
+    @FXML
+    private Button btnMenuCadastrarVeiculo;
+
+    @FXML
+    private Button btnMenuDevolucao;
+
+    @FXML
+    private Button btnMenuGerarRelatorios;
+
+    @FXML
+    private Button btnMenuLocarVeiculo;
+
+    @FXML
+    private Button btnMenuManutencoes;
+
+}
