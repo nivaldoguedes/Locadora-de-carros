f338caceed16041c5dfd47e9ee82092c0a3e913f
Ruslan <ruslan.andruscha.dev@gmail.com>
2025-11-10 11:20:20 -0300

    Adicionando tela de cadastro de veiculos e novos arquivos CSS

---
+++ src/controller/CadastroDeVeiculoController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CadastroDeVeiculoController {
+
+    @FXML
+    private Button botaoCadastrarVeiculo;
+
+    @FXML
+    private Button botaoSair;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private TextField inputAno;
+
+    @FXML
+    private TextField inputMarca;
+
+    @FXML
+    private TextField inputModelo;
+
+    @FXML
+    private TextField inputPlaca;
+
+    @FXML
+    private TextField inputQuilometragem;
+
+}
