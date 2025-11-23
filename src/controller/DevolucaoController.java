f32282ca89acf61685700f1fe2fb03949467a5a3
RuslanAndruscha <ruslan.andruscha.dev@gmail.com>
2025-11-23 09:46:51 -0300

    Criada tela de devolucao e seu controller

---
+++ src/controller/DevolucaoController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class DevolucaoController {
+
+    @FXML
+    private Button botaoConfirmarDevolucao;
+
+    @FXML
+    private Button botaoReservar1;
+
+    @FXML
+    private Button botaoSair;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private ChoiceBox<?> carroDevolvido;
+
+    @FXML
+    private ChoiceBox<?> danoCausado;
+
+    @FXML
+    private TextField quilometragemDevolucao;
+
+    @FXML
+    private Text valorDano;
+
+    @FXML
+    void clickBotaoSair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBotaoVoltar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmarDevolucaoDeCarro(ActionEvent event) {
+
+    }
+
+}
